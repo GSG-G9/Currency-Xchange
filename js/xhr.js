b84ab19f7abf0be6fdc716b914429d7393a14117
Alaa <alaa.sahloub.eb@gmail.com>
2020-11-11 16:43:51 +0200

    XHR Request & DOM for background images using unsplash API

--- js/xhr.js
+++ js/xhr.js
@@ -3,8 +3,7 @@ function GenericXHR (url , responseCallback){
     xhr.onreadystatechange = ()=>{
         if(xhr.readyState === 4){
             if(xhr.status === 200){
-                const respnose = JSON.parse(xhr.responseText);
-                responseCallback(respnose);
+               return responseCallback(JSON.parse(xhr.responseText));
             }else if(xhr.status === 404){
                 handelErorr(`not found,${xhr.status},${xhr.responseText1}`);
             }else{
@@ -16,7 +15,18 @@ function GenericXHR (url , responseCallback){
     xhr.send();
 }
 
-function handelErorr(data){
+function handelErorr(error){
     const errorP = document.getElementById('error_handel');
-    errorP.textContent = data;
+    errorP.textContent = error;
 }
+
+const inputChangeTo = document.getElementById('exchange_to_input');
+const main = document.getElementById('main');
+
+// GenericXHR( `https://api.unsplash.com/search/photos?client_id=0ZxAs6sAoEUe2x4F8jct34FDsXarlgmSj6q5o1QXa9I&query=${inputChangeTo}`, (data)=> console.log(data))
+
+GenericXHR( `https://api.unsplash.com/search/photos?client_id=0ZxAs6sAoEUe2x4F8jct34FDsXarlgmSj6q5o1QXa9I&query=london`, (data)=>makeBa(data)
+    function makePackground(imgdata){
+        console.log( data.results[0].urls.full);
+    }
+)
