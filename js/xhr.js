6698fdcc3497120c9dec221c837f87009670554e
Alaa <alaa.sahloub.eb@gmail.com>
2020-11-11 12:17:41 +0200

    edit genericXHR function and handel for 404 xhr.status and else statuses

--- js/xhr.js
+++ js/xhr.js
@@ -4,10 +4,19 @@ function GenericXHR (url , responseCallback){
         if(xhr.readyState === 4){
             if(xhr.status === 200){
                 const respnose = JSON.parse(xhr.responseText);
-                responseCallback(Response);
+                responseCallback(respnose);
+            }else if(xhr.status === 404){
+                handelErorr(`not found,${xhr.status},${xhr.responseText1}`);
+            }else{
+                handelErorr(`internal error,${xhr.status},${xhr.responseText}`);
             }
         }
     }
     xhr.open("GET" , url);
     xhr.send();
+}
+
+function handelErorr(data){
+    const errorP = document.getElementById('error_handel');
+    errorP.textContent = data;
 }
