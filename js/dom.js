5ae5b2e5c7e16fbbd62c3932ebef75ea9c8b87f1
Alaa Alser <alaalsser@gmail.com>
2020-11-11 17:28:27 +0200

    edit-dom

--- js/dom.js
+++ js/dom.js
@@ -1,25 +1,29 @@
 const exchangeFrom = document.getElementById('exchange_from_input');
 const exchangeTo = document.getElementById('exchange_to_input');
 const exchangeFromFlag = document.getElementById('country_from_flat');
-const exchangeToFlag = document.getElementById('country_to_flat');
+const exchangeToFlag = document.getElementById('country_to_flag');
 const exchangeBtn = document.getElementById('exchange_btn');
 const country_from_symbol = document.getElementById('country_from_symbol')
 const country_to_symbol = document.getElementById('country_to_symbol')
 
 const countryFunc1 = (response) => {
-  console.log(response.code);
-    exchangeFromFlag.src = response.flag
-    country_from_symbol.innerContent = response.code
+    exchangeFromFlag.src = response[0].flag
+    country_from_symbol.innerText = response[0].currencies[0].code
+
 }
 
 const countryFunc2 = (response) => {
-  exchangeToFlag.src = response.flag
-  country_to_symbol.innerContent = response.code
+  exchangeToFlag.src = response[0].flag
+  country_to_symbol.innerText = response[0].currencies[0].code
 }
 
 
-  exchangeBtn.addEventListener('click', () => {
+exchangeFrom.addEventListener('focusout', () => {
     GenericXHR(`https://restcountries.eu/rest/v2/name/${exchangeFrom.value}`,countryFunc1)
+  });
+
+ exchangeTo.addEventListener('focusout', () => {
+    GenericXHR(`https://restcountries.eu/rest/v2/name/${exchangeTo.value}`,countryFunc2)
   });
 
   
