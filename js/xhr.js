05c8e1ca6582f0f67935fefae0cef00be45655d2
Alaa <alaa.sahloub.eb@gmail.com>
2020-11-11 17:54:00 +0200

    edits for background img unsplach

--- js/xhr.js
+++ js/xhr.js
@@ -15,18 +15,8 @@ function GenericXHR (url , responseCallback){
     xhr.send();
 }
 
-function handelErorr(error){
-    const errorP = document.getElementById('error_handel');
-    errorP.textContent = error;
+
+
+module.exports = {
+    GenericXHR,
 }
-
-const inputChangeTo = document.getElementById('exchange_to_input');
-const main = document.getElementById('main');
-
-// GenericXHR( `https://api.unsplash.com/search/photos?client_id=0ZxAs6sAoEUe2x4F8jct34FDsXarlgmSj6q5o1QXa9I&query=${inputChangeTo}`, (data)=> console.log(data))
-
-GenericXHR( `https://api.unsplash.com/search/photos?client_id=0ZxAs6sAoEUe2x4F8jct34FDsXarlgmSj6q5o1QXa9I&query=london`, (data)=>makeBa(data)
-    function makePackground(imgdata){
-        console.log( data.results[0].urls.full);
-    }
-)
